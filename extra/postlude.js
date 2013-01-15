6aaa96c1742f94b9042c5586c3637a9475b3ae10
Bjørge Næss <bjoerge@bengler.no>
2013-01-15 19:32:27 +0100

    Export using jQuery.noConflict()

--- extra/postlude.js
+++ extra/postlude.js
@@ -1,4 +1,5 @@
 
+  window.jQuery.noConflict();
   if (typeof exports != 'undefined' && typeof module.exports != 'undefined') module.exports = window.jQuery;
 
 }(this, typeof window === 'undefined' ? undefined : window));
